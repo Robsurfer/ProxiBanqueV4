e4452895757ccfe1a26c16ade0d15fa4de73effc
Loriane EYMANN <loriane.eymann@gmail.com>
2018-01-09 13:20:10 +0100

    Correction CompteController

--- ProxibanqueV4/proxibanque-webservice/src/main/java/fr/gtm/proxibanque/webservice/CompteController.java
+++ ProxibanqueV4/proxibanque-webservice/src/main/java/fr/gtm/proxibanque/webservice/CompteController.java
@@ -1,10 +1,6 @@
 package fr.gtm.proxibanque.webservice;
 
-<<<<<<< HEAD
-import java.util.Collection;
-=======
 import java.util.List;
->>>>>>> WebServices
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,28 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 import fr.gtm.proxibanque.dao.ICompteDao;
 import fr.gtm.proxibanque.domaine.Compte;
 
-<<<<<<< HEAD
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class CompteController {
 	
 	@Autowired
 	private ICompteDao compteDao;
-	
-	@RequestMapping(value = "conseiller/{login}/clients/{id}/comptes", method = RequestMethod.GET, produces="application/json")
-	public Collection<Compte> getAll(@PathVariable("id") int id, @PathVariable("login") String login)
-	{
-		return compteDao.findByClient_id(id);
-	}
 
-=======
-
-@RestController
-@CrossOrigin(origins = "http://localhost:4200")
-public class CompteController {
-
-	@Autowired
-	private ICompteDao compteDao;
 	
 	@RequestMapping(value = "conseiller/{login}/clients/comptes", method = RequestMethod.GET, produces = "application/json")
 	public List<Compte> getAll(@PathVariable("login") String login) {
@@ -48,5 +29,4 @@ public class CompteController {
 	public List<Compte> getById(@PathVariable("id") int id, @PathVariable("login") String login) {
 		return compteDao.findByClient_idAndClient_Conseiller_login(id, login);
 	}
->>>>>>> WebServices
 }
