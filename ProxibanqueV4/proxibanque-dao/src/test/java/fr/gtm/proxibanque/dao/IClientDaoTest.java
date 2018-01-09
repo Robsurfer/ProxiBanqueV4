f4e728d54652168e43157bdf64e7cb4598bcfdcc
Liyuki <rodier.lise@gmail.com>
2018-01-09 12:51:24 +0100

    Modif application.properties dans la dao au lieu de webservice

---
+++ ProxibanqueV4/proxibanque-dao/src/test/java/fr/gtm/proxibanque/dao/IClientDaoTest.java
@@ -0,0 +1,23 @@
+package fr.gtm.proxibanque.dao;
+
+/*import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IClientDaoTest {
+
+    @Autowired
+    private IClientDao clientDao;
+
+    @Test
+    public void contexLoads() throws Exception {
+        assertThat(clientDao).isNotNull();
+    }
+}*/
