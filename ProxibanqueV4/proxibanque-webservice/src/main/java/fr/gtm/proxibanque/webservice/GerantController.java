02392df58a9d4925b2e6eba6eb0648012de4b05f
Rob <robinson.bord@wanadoo.fr>
2018-01-09 16:05:43 +0100

    WS recup virement entre 2 dates
    WS recup virement entre 2 dates dans le GerantController

--- ProxibanqueV4/proxibanque-webservice/src/main/java/fr/gtm/proxibanque/webservice/GerantController.java
+++ ProxibanqueV4/proxibanque-webservice/src/main/java/fr/gtm/proxibanque/webservice/GerantController.java
@@ -1,5 +1,8 @@
 package fr.gtm.proxibanque.webservice;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
@@ -22,13 +25,25 @@ public class GerantController {
 	@Autowired
 	private IVirementDao virementDao;
 
-	@RequestMapping(value = "/audit", method = RequestMethod.GET, produces = "application/json")
-	public List<Virement> Audit(@PathVariable("date1") Date date1, @PathVariable("date2") Date date2) {
-		return virementDao.findByDateVirementBeforeAndDateVirementAfter(date1, date2);
+	@RequestMapping(value = "conseiller/audit/{date1}/{date2}", method = RequestMethod.GET, produces = "application/json")
+	public List<Virement> Audit(@PathVariable("date1") String date1, @PathVariable("date2") String date2) {
+		 DateFormat input = new SimpleDateFormat("ddMMyyyy");
+		 DateFormat output = new SimpleDateFormat("dd/MM/yyyy");
+
+			Date dateForm1 = new Date();
+			Date dateForm2 = new Date();
+		try {
+		
+			dateForm1 = input.parse(date1);
+			dateForm2 = input.parse(date2);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return virementDao.findByDateVirementBeforeAndDateVirementAfter(dateForm1, dateForm2);
 	}
-	
-	//public Collection<Virement> findAllByConseiller_login(String login);
-	
+		
 	@RequestMapping(value = "conseiller/{login}/virements", method = RequestMethod.GET, produces = "application/json")
 	public Collection<Virement> getAll(@PathVariable("login") String login) {
 		return virementDao.findAllByConseiller_login(login);
