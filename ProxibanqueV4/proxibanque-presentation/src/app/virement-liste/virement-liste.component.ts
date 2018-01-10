10e53765049693cf9298d200b92a84453aaa3e69
Loriane EYMANN <loriane.eymann@gmail.com>
2018-01-10 10:15:21 +0100

    correction module router

--- ProxibanqueV4/proxibanque-presentation/src/app/virement-liste/virement-liste.component.ts
+++ ProxibanqueV4/proxibanque-presentation/src/app/virement-liste/virement-liste.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../login.service';
-import { Router } from '@angular/router/src/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-virement-liste',
