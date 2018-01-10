ec6c117e2b0021ecf44c3881ceef207a8edf035d
Rob <robinson.bord@wanadoo.fr>
2018-01-10 12:14:09 +0100

    debut liste virement controller

--- ProxibanqueV4/proxibanque-presentation/src/app/virement-liste/virement-liste.component.ts
+++ ProxibanqueV4/proxibanque-presentation/src/app/virement-liste/virement-liste.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Virement } from '../virement';
+import { ListeVirementService } from '../listevirement.service';
+import { LoginService } from '../login.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-virement-liste',
   templateUrl: './virement-liste.component.html',
@@ -7,6 +10,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VirementListeComponent implements OnInit {
 
+  //Liste des virements
+  virements: Virement[];
   constructor() { }
 
   ngOnInit() {
